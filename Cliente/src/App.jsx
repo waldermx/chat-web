7dc25de3279f94e58eb12ddadc96942a551ca17b
Emilio Gómez <gotreowen@gmail.com>
2023-06-30 17:57:36 -0600

    ignore socketio

--- Cliente/src/App.jsx
+++ Cliente/src/App.jsx
@@ -3,8 +3,8 @@ import './App.css'
 import Chat from './Layout/Chat'
 import ChatBar from './Components/ChatBar'
 import BottomBar from './Layout/BottomBarAndConfig'
-import io from 'socket.io-client'
-import { PORT } from '../../Server/config'
+// import io from 'socket.io-client'
+// import { PORT } from '../../Server/config'
 
 // const socket = io(`http://localhost:${PORT}`)
 
